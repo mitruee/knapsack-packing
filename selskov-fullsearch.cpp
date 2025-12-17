8b6a69d7b10098d4afebbdd7a2404236c155b810
BradObshePitt <mitya_slasher@mail.ru>
2025-12-17 21:33:38 +0300

    Added comments

--- selskov-fullsearch.cpp
+++ selskov-fullsearch.cpp
@@ -15,12 +15,14 @@ vector<vector<int>> fullsearch_method(Knapsack knapsack)
     vector<int> bestItems; 
     
     int totalCombinations = 1 << n;
-    
+
+        // Перебор всех масок (битовых комбинаций)
     for (int mask = 0; mask < totalCombinations; ++mask) {
         int currentWeight = 0;
         int currentValue = 0;
         vector<int> currentItems;
         
+     // Проверка включенных в маску предметов
         for (int i = 0; i < n; ++i) {
             if (mask & (1 << i)) {
                 currentWeight += items[i][0];
@@ -29,12 +31,14 @@ vector<vector<int>> fullsearch_method(Knapsack knapsack)
             }
         }
         
+        //Проверка вместимости и стоимости
         if (currentWeight <= capacity && currentValue > maxValue) {
             maxValue = currentValue;
-            bestItems = currentItems;  // ← убрали maxWeight
+            bestItems = currentItems; 
         }
     }
       
+    //Форматирование результата
     for (int idx : bestItems) {
         result.push_back({items[idx][0], items[idx][1]});
     }
