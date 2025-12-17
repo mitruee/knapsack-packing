f320024c890367fbefd33827b4f477a2a45d8559
Evinedim <nikvoron3008@gmail.com>
2025-12-17 23:05:48 +0300

    The recursive method was changed to iterative

--- vorontsov-dp.cpp
+++ vorontsov-dp.cpp
@@ -5,44 +5,32 @@
 vector<vector<int>> dp_method(Knapsack knapsack)
 {
 	vector<vector<int>> items = knapsack.getItems();
-	vector<int> weights, prices;
 
-	for (auto& item : items) {
-		weights.push_back(item[0]);
-		prices.push_back(item[1]);
-	}
+	int n = knapsack.getItemsSize();
+	int W = knapsack.getCapacity();
+	vector<vector<int>> table(n + 1, vector<int>(W + 1, 0));
 
-	int itemsSize = knapsack.getItemsSize();
-	int capacity = knapsack.getCapacity();
-	vector<vector<int>> table(itemsSize + 1, vector<int>(capacity + 1, -1));
-
-	function<int(int, int)> fillingKnapsack;
-
-	fillingKnapsack = [&weights, &prices, &table, &fillingKnapsack](int k, int s) {
-		if (k == 0 || s == 0) { return 0; }
-
-		if (table[k][s] != -1) { return table[k][s]; }
-
-		if (weights[k - 1] > s) {
-			table[k][s] = fillingKnapsack(k - 1, s);
-		} else {
-			table[k][s] = max(fillingKnapsack(k - 1, s), 
-				fillingKnapsack(k - 1, s - weights[k - 1]) + prices[k - 1]);
-		}
-
-		return table[k][s];
-	};
-
-	fillingKnapsack(itemsSize, capacity);
+	for (int k = 1; k <= n; k++) {
+        int weight = items[k - 1][0];
+        int price = items[k - 1][1];
+        
+        for (int s = 0; s <= W; s++) {
+            if (weight > s) {
+                table[k][s] = table[k - 1][s];
+            } else {
+                table[k][s] = max(table[k - 1][s], table[k - 1][s - weight] + price);
+            }
+        }
+    }
 
 	vector<vector<int>> result;
 	
-	while (itemsSize > 0 && capacity > 0) {
-		if (table[itemsSize][capacity] != table[itemsSize - 1][capacity]) {
-			result.push_back({weights[itemsSize - 1], prices[itemsSize - 1]});
-			capacity -= weights[itemsSize - 1];
+	while (n > 0 && W > 0) {
+		if (table[n][W] != table[n - 1][W]) {
+			result.push_back(items[n - 1]);
+			W -= items[n - 1][0];
 		}
-		itemsSize--;
+		n--;
 	}
 
 	return result;
